54ce0a788a91b387db2b34a54bacc945e1484366
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-14 12:57:13 -0600

    commit tree edfd39391ebe3cbecf1346fd337afddeaed23371

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  create_an_array = []
 end
 
 def create_an_array
-  
+  create_an_array = ["egg" , "bacon" , "toast", "wow"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    create_an_array = ["egg" , "bacon" , "toast", "wow"]
+    create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    create_an_array = ["egg" , "bacon" , "toast", "juice"]
+    create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    create_an_array = ["egg" , "bacon" , "toast", "arrays!"]
+    juice = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    create_an_array = ["wow" , "bacon" , "toast", "arrays!"]
+    egg = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    create_an_array = ["egg" , "bacon" , "toast", "am"]
+    create_an_array[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    create_an_array = ["wow" , "bacon" , "toast", "wow"]
+    create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    create_an_array = ["wow" , "bacon" , "toast", "arrays!"]
+    create_an_array[3]
 end
